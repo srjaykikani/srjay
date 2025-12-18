aee5dcf8c38d0eb8e08d84020c452fa9a1ac7327
S R Jay <srjaykikani@gmail.com>
2025-12-18 19:28:47 +0530

    Revert "Update next.config.mjs"
    This reverts commit 5ab5c28276533346e2aed6ced644d1b172fc92fb.

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
